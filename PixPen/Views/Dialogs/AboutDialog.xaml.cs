b0f172c940b8117f19da441060581b36130dc984
azulamb <1127785+azulamb@users.noreply.github.com>
2026-06-03 19:32:20 +0900

    Fix version.

--- PixPen/Views/Dialogs/AboutDialog.xaml.cs
+++ PixPen/Views/Dialogs/AboutDialog.xaml.cs
@@ -1,9 +1,19 @@
+using System.Reflection;
 using System.Windows;
 
 namespace PixPen.Views.Dialogs;
 
 public partial class AboutDialog : Window
 {
-    public AboutDialog() => InitializeComponent();
+    public AboutDialog()
+    {
+        InitializeComponent();
+        var version = Assembly.GetExecutingAssembly()
+            .GetName().Version;
+        VersionText.Text = version != null
+            ? $"PixPen v{version.Major}.{version.Minor}.{version.Build}"
+            : "PixPen";
+    }
+
     private void OnClose(object sender, RoutedEventArgs e) => Close();
 }
